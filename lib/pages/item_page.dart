34bb9faf46e4250efbd20822d5ed1ec1990d7749
MariskaPutri18 <mariskaputri960@gmail.com>
2025-10-07 19:39:31 +0700

    praktikum 5: navigation

---
+++ lib/pages/item_page.dart
@@ -0,0 +1,19 @@
+import 'package:belanja/models/item.dart';
+import 'package:flutter/material.dart';
+
+class ItemPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final item = ModalRoute.of(context)!.settings.arguments as Item;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Shopping List")),
+      body: Center(
+        child: Text(
+          '${item.name} with ${item.price}',
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
